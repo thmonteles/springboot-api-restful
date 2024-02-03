a5b35be034d95b35c62602bb742504507da769c8
Thaina Monteles <thmonteles@gmail.com>
2024-02-03 18:55:50 -0300

    API Restful considerações iniciais

---
+++ src/main/java/com/example/springboot/controllers/HomeController.java
@@ -0,0 +1,23 @@
+package com.example.springboot.controllers;
+
+import com.example.springboot.Dtos.ProductRecordDto;
+import com.example.springboot.models.ProductModel;
+import com.example.springboot.repositories.ProductRepository;
+import jakarta.validation.Valid;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public  String home(){
+        return "hello world";
+    }
+}
